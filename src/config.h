07730e5220a939fd31faed26613091160b23ed72
Sapein <sapeint@hashbang.sh>
2018-08-05 21:22:43 -0400

    Update config file to match new requirements

--- src/config.h
+++ src/config.h
@@ -4,4 +4,8 @@
 #define DE_MAX_PLAYERS_IN_DUNGEON 5
 #define DE_MAX_MEMBUFF_SIZE
 
+/* Can either be NONE or SQLITE3 */
+#define DE_DATABASE_TYPE SQLITE3
+#define DE_DATABASE_PATH "dungeonbot.db"
+
 #endif
