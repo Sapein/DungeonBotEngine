66cb2bddae8512c2c3ff7e7f8fea584342bbf386
Sapein <sapeint@hashbang.sh>
2018-08-08 23:43:17 -0400

    Complete Player Function Implementation for DB.
    The Player Series of Functions is now, mostly, completed. The
    UpdatePlayer is going to be written last, due to it being one of the
    more difficult things.
    
    The Character Series of Functions is now getting there, with the ability
    to register Characters already implemeented.

--- src/database.c
+++ src/database.c
@@ -1,25 +1,39 @@
 #include "database.h"
 #include "config.h"
+#include "players.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #if (DE_DATABASE_TYPE == SQLITE3)
 #include <sqlite3.h>
 #define CHECK_EMPTY "SELECT name FROM sqlite_master;\0"
 #define CREATE_ACCOUNTS "CREATE TABLE Accounts(account_id INTEGER Primary Key, name TEXT, password BLOB);\0"
 #define CREATE_CHARACTERS "CREATE TABLE Characters(character_id INTEGER Primary Key, name TEXT, information BLOB);\0"
 #define CREATE_PLAYER_CHARS "CREATE TABLE Character_Owners(account_id INTEGER, character_id INTEGER);\0"
-#define STATEMENT_COUNT 15
+#define STATEMENT_COUNT 18
 #endif
 
-/* Important Functions */
+/* Stuff from players.c */
+struct DungeonEngine_Player {
+    struct DungeonEngine_PlayerInfo *;
+    int session;
+    int owner_id;
+};
+
+
+/* Internal Goodies*/
 static enum SQL_MODE {NONE, SQLITE3};
 static enum SQL_MODE mode = DE_DATABASE_TYPE;
 statis enum SQL_Statements {REGISTER_PLAYER, REGISTER_CHARACTER, REGISTER_OWNERSHIP,
                             DELETE_PLAYER, DELETE_CHARACTER, DELETE_CHARACTER_OWNERSHIP, DELETE_ACCOUNT_OWNERSHIP,
                             UPDATE_CHARACTER_NAME, UPDATE_CHARACTER_INFORMATION,
-                            GET_ACCOUNT_BY_PASS, GET_ACCOUNT_BY_NAME, GET_ACCOUNT_BY_PASSNAME
-                            GET_PASS_BY_NAME, GET_CHARACTER_BY_OWNER, GET_CHARACTER_BY_NAME};
+                            GET_ACCOUNT_BY_PASS, GET_ACCOUNT_BY_NAME, GET_ACCOUNT_BY_PASSNAME,
+                            GET_PASS_BY_NAME, GET_CHARACTER_BY_OWNER, GET_CHARACTER_BY_NAME,
+                            GET_CHARCTER_BY_INFO, GET_CHARACTER_BY_NAMEINFO,
+                            GET_NAME_BY_ACCOUNT};
+
+/* The Engine's Databse 'Object' */
 static union {
     sqlite3 *sqlite3db = NULL;
     int nodb;
@@ -39,7 +53,10 @@ static char *Statements[STATEMENT_COUNT] = {"INSERT INTO Accounts(name, password
                                             "SELECT account_id FROM Accounts WHERE password=? AND name=?;\0",
                                             "SELECT password FROM Accounts WHERE name=?;\0",
                                             "SELECT character_id FROM Character_Owners WHERE account_id=?;\0",
-                                            "SELECT character_id FROM Characters WHERE name=?;\0"
+                                            "SELECT character_id FROM Characters WHERE name=?;\0",
+                                            "SELECT character_id FROM Characters WHERE information=?;\0",
+                                            "SELECT character_id FROM Characters WHERE name=? and information=?;\0",
+                                            "SELECT name FROM Accounts WHERE account_id=?;\0"
                              };
 static sqlite3_stmt *Common_Statements[STATEMENT_COUNT] = {NULL, NULL, NULL, NULL, NULL, NULL,
                                                            NULL, NULL, NULL, NULL, NULL, NULL, NULL};
@@ -108,20 +125,21 @@ enum DungeonEngine_DBErr DungeonEngine_DBShutdown(void){
 /* Player Functions */
 enum DungeonEngine_DBErr DungeonEngine_DBRegisterPlayer(char playername, long int name_len,
                                                         char password, long int password_len){
-    #if (DE_DATABSE_TYPE == SQLITE3)
+    #if (DE_DATABASE_TYPE == SQLITE3)
     sqlite3_bind_text(Common_Statements[REGISTER_PLAYER], 1, playername, name_len, SQLITE_STATIC);
     sqlite3_bind_blob(Common_Statements[REGISTER_PLAYER], 2, password, password_len, SQLITE_STATIC);
     sqlite3_step(Common_Statements[REGISTER_PLAYER]);
     sqlite3_clear_bindings(Common_Statements[REGISTER_PLAYER]);
     sqlite3_reset(Common_Statements[REGISTER_PLAYER]);
-    #elif (DE_DATABSE_TYPE == NONE) #error ** NOT IMPLEMENTED USE SQLITE3 **
+    #elif (DE_DATABASE_TYPE == NONE)
+    #error ** NOT IMPLEMENTED USE SQLITE3 **
     #endif
 }
 
 enum DungeonEngine_DBErr DungeonEngine_DBDeletePlayer(char playername, long int name_len,
                                                       char password, long int password_len){
     int success = SUCCESS;
-    #if (DE_DATABSE_TYPE == SQLITE3)
+    #if (DE_DATABASE_TYPE == SQLITE3)
     unsigned long int account_id = 0;
     char *stored_pass = NULL;
     int stored_pass_length = 0;
@@ -175,31 +193,153 @@ enum DungeonEngine_DBErr DungeonEngine_DBDeletePlayer(char playername, long int 
     }else{
         success = INVALID_LOGIN;
     }
-    #elif (DE_DATABSE_TYPE == NONE)
+    #elif (DE_DATABASE_TYPE == NONE)
     #error ** NOT IMPLEMENTED USE SQLITE3 **
     #endif
-    return SUCCESS;
+    return success;
 }
 
-enum DungeonEngine_DBErr DungeonEngine_DBGetPlayerName(char playername, long int name_len, void *player_data);
-enum DungeonEngine_DBErr DungeonEngine_DBGetPlayerId(long int player_id, void *player_data);
+enum DungeonEngine_DBErr DungeonEngine_DBGetPlayerName(unsigned long int player_id, char *playername, long int *name_len);
+    int success = SUCCESS;
+    char *name = NULL;
+    int len = 0;
+    #if (DE_DATABASE_TYPE == SQLITE3)
+    sqlite3_bind_int(Common_Statements[GET_NAME_BY_ACCOUNT], 1, player_id);
+    sqlite3_step(Common_Statements[GET_NAME_BY_ACCOUNT]);
+    name = sqlite3_column_text(Common_Statements[GET_NAME_BY_ACCOUNT], 1);
+    len = sqlite3_column_bytes(Common_Statements[GET_NAME_BY_ACCOUNT], 1);
+    memcpy(playername, name, len);
+    *name_len = len;
+    sqlite3_clear_bindings(Common_Statements[GET_NAME_BY_ACCOUNT]);
+    sqlite3_reset(Common_Statements[GET_NAME_BY_ACCOUNT]);
+    #elif (DE_DATABASE_TYPE == NONE)
+    #error ** NOT IMPLEMENTED USE SQLITE3 **
+    #endif
+    return success;
+}
+
+enum DungeonEngine_DBErr DungeonEngine_DBGetPlayerId(long int *player_id, char *player_name, unsigned long int name_len){
+    int success = SUCCESS;
+    #if (DE_DATABASE_TYPE == SQLITE3)
+    sqlite3_bind_text(Common_Statements[GET_ACCOUNT_BY_NAME], 1, player_name, name_len, SQLITE_STATIC);
+    sqlite3_step(Common_Statements[GET_ACCOUNT_BY_NAME]);
+    *player_id = sqlite3_column_int(Common_Statements[GET_ACCOUNT_BY_NAME], 1);
+    #elif (DE_DATABASE_TYPE == NONE)
+    #error ** NOT IMPLEMENTED USE SQLITE3 **
+    #endif
+    return success;
+}
 enum DungeonEngine_DBErr DungeonEngine_DBUpdatePlayer(char playername, long int name_len,
                                                       char password, long int password,
-                                                      void *new_player_data);
+                                                      void *new_player_data){
+    int success = SUCCESS;
+    #if (DE_DATABASE_TYPE == SQLITE3)
+    #warning ** DungeonEngine_DBUpdatePlayer Does Nothing in SQLITE3 Mode **
+    #elif (DE_DATABASE_TYPE == NONE)
+    #error ** NOT IMPLEMENTED USE SQLITE3 **
+    #endif
+    return success;
+}
 
 /* Character Functions */
-enum DungeonEngine_DBErr DungeonEngine_DBAddCharacter(char playername, long int name_len,
-                                                      char password, long int password_len,
-                                                      void *player_data);
+enum DungeonEngine_DBErr DungeonEngine_DBAddCharacter(char playername, long int name_len, char password, long int password_len,
+                                                      void *player_data){
+    int success = SUCCESS:
+    #if (DE_DATABASE_TYPE == SQLITE3)
+    struct DungeonEngine_PlayerInfo pInfo = *((struct DungeonEngine_PlayerInfo *)player_data);
+    unsigned long int account_id = 0;
+    unsigned long int character_id = 0;
+    char *stored_pass = NULL;
+    int stored_pass_length = 0;
+    _Bool found_pass = true;
+
+    sqlite3_bind_text(Common_Statements[GET_PASS_BY_NAME], 1, playername, name_len, SQLITE_STATIC);
+    sqlite3_step(Common_Statements[GET_PASS_BY_NAME]);
+    for(int i = 0; i > sqlite3_column_count(Common_Statements[GET_PASS_BY_NAME]) || found_pass == true; i++){
+        stored_pass_length = sqlite3_column_bytes(Common_Statements[GET_PASS_BY_NAME], i) + 1;
+        stored_pass = sqlite3_column_blob(Common_Statements[GET_PASS_BY_NAME], i);
+        if(stored_pass_length == password_len){
+            if(memcmp(password, stored_pass_length, password_len) == 0){
+                found_pass = true;
+            }else{
+                continue;
+            }
+        }
+    }
+    sqlite3_clear_bindings(Common_Statements[GET_PASS_BY_NAME]);
+    sqlite3_reset(Common_Statements[GET_PASS_BY_NAME]);
+
+    if(found_pass){
+        sqlite3_bind_blob(Common_Statements[GET_ACCOUNT_BY_PASSNAME], 1, password, password_length, SQLITE_STATIC);
+        sqlite3_bind_text(Common_Statements[GET_ACCOUNT_BY_PASSNAME], 2, playername, name_len, SQLITE_STATIC);
+        sqlite3_step(Common_Statements[GET_ACCOUNT_BY_PASSNAME]);
+        account_id = sqlite3_column_int(Common_Statements[GET_ACCOUNT_BY_PASSNAME], 1);
+        sqlite3_clear_bindings(Common_Statements[GET_ACCOUNT_BY_PASSNAME]);
+        sqlite3_reset(Common_Statements[GET_ACCOUNT_BY_PASSNAME]);
+
+        sqlite3_bind_text(Common_Statements[REGISTER_CHARACTER], 1, pInfo.character_name, pInfo.char_name_len, SQLITE_STATIC);
+        sqlite3_bind_blob(Common_Statements[REGISTER_CHARACTER], 2, pInfo,
+                          (sizeof(pInfo) + (pInfo.char_name_len * sizeof(char))), SQLITE_STATIC);
+        sqlite3_step(Common_Statements[REGISTER_CHARACTER]);
+        sqlite3_clear_bindings(Common_Statements[REGISTER_CHARACTER]);
+        sqlite3_reset(Common_Statements[REGISTER_CHARACTER]);
+
+        sqlite3_bind_text(Common_Statements[GET_CHARACTER_BY_NAMEINFO], 1, pInfo.character_name, pInfo.char_name_len, SQLITE_STATIC);
+        sqlite3_bind_blob(Common_Statements[GET_CHARACTER_BY_NAMEINFO], 2, pInfo,
+                          sizeof(pInfo) + (pInfo.char_name_len * sizeof(char)), SQLITE_STATIC);
+        sqlite3_step(Common_Statements[GET_CHARACTER_BY_NAMEINFO]);
+        character_id = sqlite3_column_int(Common_Statements[GET_CHARACTER_BY_NAMEINFO], 1);
+        sqlite3_clear_bindings(Common_Statements[GET_CHARACTER_BY_NAMEINFO]);
+        sqlite3_reset(Common_Statements[GET_CHARACTER_BY_NAMEINFO]);
+
+        sqlite3_bind_int(Common_Statements[REGISTER_OWNERSHIP], 1, account_id);
+        sqlite3_bind_int(Common_Statements[REGISTER_OWNERSHIP], 2, character_id);
+        sqlite3_step(Common_Statements[REGISTER_OWNERSHIP]);
+        sqlite3_clear_bindings(Common_Statements[REGISTER_OWNERSHIP]);
+        sqlite3_reset(Common_Statements[REGISTER_OWNERSHIP]);
+    }else if(!found_pass){
+        success = INVALID_LOGIN;
+    }
+    #elif (DE_DATABASE_TYPE == NONE)
+    #error ** NOT IMPLEMENTED USE SQLITE3 **
+    #endif
+    return success;
+}
+
 enum DungeonEngine_DBErr DungeonEngine_DBDeleteCharacter(char playername, long int name_len,
                                                          char password, long int password_len,
-                                                         void *player_data);
+                                                         void *player_data){
+    #if (DE_DATABASE_TYPE == SQLITE3)
+    #error ** NOT IMPLEMENTED YET**
+    #elif (DE_DATABASE_TYPE == NONE)
+    #error ** NOT IMPLEMENTED USE SQLITE3 **
+    #endif
+
+}
 enum DungeonEngine_DBErr DungeonEngine_DBGetCharacterName(char playername, long int name_len,
-                                                          void *player_data);
-enum DungeonEngine_DBErr DungeonEngine_DBGetCharacterId(long int character_id);
+                                                          void *player_data){
+    #if (DE_DATABASE_TYPE == SQLITE3)
+    #error ** NOT IMPLEMENTED YET**
+    #elif (DE_DATABASE_TYPE == NONE)
+    #error ** NOT IMPLEMENTED USE SQLITE3 **
+    #endif
+}
+enum DungeonEngine_DBErr DungeonEngine_DBGetCharacterId(long int character_id){
+    #if (DE_DATABASE_TYPE == SQLITE3)
+    #error ** NOT IMPLEMENTED YET**
+    #elif (DE_DATABASE_TYPE == NONE)
+    #error ** NOT IMPLEMENTED USE SQLITE3 **
+    #endif
+}
 enum DungeonEngine_DBErr DungeonEngine_DBUpdatePlayer(char charactername, long int name_len,
                                                       char password, long int password,
-                                                      void *new_char_data);
+                                                      void *new_char_data){
+    #if (DE_DATABASE_TYPE == SQLITE3)
+    #error ** NOT IMPLEMENTED YET**
+    #elif (DE_DATABASE_TYPE == NONE)
+    #error ** NOT IMPLEMENTED USE SQLITE3 **
+    #endif
+}
 
 
 /* Misc. Functions */
