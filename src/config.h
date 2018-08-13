51bb1eb1968e0fd558bcf025ff682bd5ff47ec7b
Sapein <sapeint@hashbang.sh>
2018-08-12 20:50:44 -0400

    Change a define

--- src/config.h
+++ src/config.h
@@ -1,11 +1,10 @@
 #ifndef DECONFIG
 #define DECONFIG
 
+#define DE_MAX_CHARACTERS_LOADABLE  10
 #define DE_MAX_PLAYERS_IN_DUNGEON 5
-#define DE_MAX_MEMBUFF_SIZE
 
 /* Can either be NONE or SQLITE3 */
 #define DE_DATABASE_TYPE SQLITE3
 #define DE_DATABASE_PATH "dungeonbot.db"
-
 #endif
