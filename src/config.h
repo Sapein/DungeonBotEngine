f8bdd8b01bfb844380b37a21ef4a6e2211eed2e4
Sapein <sapeint@hashbang.sh>
2018-09-02 15:19:22 -0400

    Add in new Configuration for Dungeons

--- src/config.h
+++ src/config.h
@@ -4,6 +4,15 @@
 #define DE_MAX_CHARACTERS_LOADABLE  10
 #define DE_MAX_PLAYERS_IN_DUNGEON 5
 
+/* Dungeon Generation Constants */
+/* DE_MAX_ROOMS is the absolute maximum number of rooms that can be generated */
+/* DE_GENERATION_HEIGHT and DE_GENERATION_WIDTH are used in the generaiton process,
+ *  it is advised to make sure that DE_GENERATION_HEIGHT plus DE_GENERATION_WIDTH are not less
+ *  than DE_MAX_ROOMS */
+#define DE_MAX_ROOMS 100
+#define DE_GENERATION_HEIGHT 100
+#define DE_GENERATION_WIDTH  100
+
 /* Can either be NONE or SQLITE3 */
 #define DE_DATABASE_TYPE SQLITE3
 #define DE_DATABASE_PATH "dungeonbot.db"
